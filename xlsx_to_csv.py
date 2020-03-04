eb7ff49167689925942dea32c2c67b6744f9db4b
erahularora <erahularora@github.com>
2020-03-04 22:12:13 +1100

    First commit

---
+++ xlsx_to_csv.py
@@ -0,0 +1,26 @@
+import xlrd
+import csv
+from datetime import datetime
+
+xl_file = "C:\Rahul\TestFile\Sample.xlsx"
+
+workbook = xlrd.open_workbook(xl_file)
+
+sheet = workbook.sheets()[0]
+
+csvname = "C:\Rahul\TestFile\Converted.csv"
+cf = open(csvname, "w", newline='\n')
+cw = csv.writer(cf)
+
+for r in range(0, sheet.nrows):
+    row = []
+    for cell in sheet.row(r):
+        if isinstance(cell.value, float):
+            row.append(datetime(*xlrd.xldate_as_tuple(cell.value, 0)).strftime('%d/%m/%Y'))
+        else:
+            row.append(cell.value)
+    cw.writerow(row)
+
+workbook.release_resources()
+del workbook
+cf.close()
